9be9608b61731e5eb0df171145e01fd0892cef16
dongja94 <dongja94@gmail.com>
2016-02-05 14:45:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplenavermovie/ParseException.java
@@ -0,0 +1,14 @@
+package com.example.dongja94.samplenavermovie;
+
+/**
+ * Created by dongja94 on 2016-02-05.
+ */
+public class ParseException extends Exception {
+    public ParseException() {
+        super();
+    }
+
+    public ParseException(String message) {
+        super(message);
+    }
+}
